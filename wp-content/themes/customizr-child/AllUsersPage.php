316dc6540654588949a97da432c2551d1c3258bd
edwardlach <edwarddlach@gmail.com>
2014-06-04 11:55:46 -0400

    Adding ajax to page
    Worked on adding ajax to doc, not working yet, I will continue to screw
    around until it does though

--- wp-content/themes/customizr-child/AllUsersPage.php
+++ wp-content/themes/customizr-child/AllUsersPage.php
@@ -5,6 +5,59 @@ Template Name: All Brogeys
 ?>
 
 
+<script language="javascript" type="text/javascript">
+<!--
+//Browser Support Code
+
+function ajaxFunction() {
+
+	var ajaxRequest; //The variable that makes ajax possible
+	
+	try {
+		//Opera8.0, Firefox, Safari
+		ajaxRequest = new XMLHttpRequest();	
+	} catch(e) {
+		//Internet Explorer Browsers
+		try {
+			ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
+		} catch(e) {
+			try {
+				ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
+			} catch(e) {
+			// Something went wrong
+				alert("Your browser broke!");
+				return false;
+			}
+		}
+	}
+			
+	//Create a function that will receive data from the server
+	ajaxRequest.onreadystatechange = function() {
+		if (ajaxRequest.readystate == 4) {
+			document.myForm.time.value = ajaxRequest.responseText;
+		}
+	}
+	
+	var is_favorited = document.getElementById('is_favorited').value;
+	var friended_by_user_id = <?php get_current_user_id(); ?>
+	var friend_user_id = document.getElementById('friend_user_id').value;
+	var friend_user_name = document.getElementById('friend_user_name').value;
+	
+	var queryString =  "admin-ajax.php?is_favorited=" + is_favorited + "&friended_by_user_id=" + friended_by_user_id + 
+	"&friend_user_id=" + friend_user_id + "&friend_user_name=" + friend_user_name;
+	
+	ajaxRequest.open("GET", queryString, true);
+	ajaxRequest.send(); 
+
+}
+
+//-->
+</script>
+
+
+
+
+
 
 <?php
     ## TODO Move this to a stylesheet
@@ -35,7 +88,7 @@ EOCSS;
         ## But out database will be safe
         ## So just don't change the blog prefix to something ridiculous
         $prefix = addslashes( $wpdb->get_blog_prefix() );
-        $tables = array_map( function($t) { return "$t"; }, array('users') );
+        $tables = array_map( function($t) { return "$t"; }, array('users', 'favorited_friends') );
         foreach( $tables as $table ) {
             if ( !isset( $wpdb->$table ) ) {
                 $wpdb->$table = "$prefix$table";
@@ -74,16 +127,83 @@ EOCSS;
 
     $wpdb = _prepare_bg_database();
     	
+
+
 	$allBrogeyUsers = $wpdb->get_results(
 		$wpdb->prepare(
-		    "SELECT S.*, user_login
+		    "SELECT S.*, ID, user_login
 		     FROM $wpdb->users S",
 		    get_current_user_id()
 		), OBJECT_K
 	);
+	
+
+		// Retrieve data from Query String
+	$is_favorited = $_GET['is_favorited'];
+	$friended_by_user_id = $_GET['friended_by_user_id'];
+	$friend_user_id = $_GET['friend_user_id'];
+	$friend_user_name = $_GET['friend_user_name'];
+		// Escape User Input to help prevent SQL Injection
+	$is_favorited = mysql_real_escape_string($is_favorited);
+	$friended_by_user_id = mysql_real_escape_string($friended_by_user_id);
+	$friend_user_id = mysql_real_escape_string($friend_user_id);
+	$friend_user_name = mysql_real_escape_string($friend_user_name);	
+		
+
+/* //Query built to display information on page after it's updated in database?	
+	
+		
+		//build query
+	if(!is_null($is_favorited)
+		$query = "SELECT * FROM $wpdb->favorited_friends WHERE is_favorited = '$is_favorited' AND friended_by_user_id = '$friended_by_user_id
+			  AND friend_user_id = '$friend_user_id' AND friend_user_name = '$friend_user_name'";
+	
+		//Execute query
+	$qry_result = mysql_query($query) or die(mysql_error());
+	
+*/	
 
 
-    $html = generate_table( $allBrogeyUsers );
+function record($parameters) {
+        ## Should save to DB
+        $wpdb = _prepare_bg_database();
+
+        $wpdb->insert(
+            $wpdb->bg_favorited_friends,
+            array(
+                'is_favorited'        => $is_favorited,
+                'friended_by_user_id' => $friended_by_user_id,
+                'friend_user_id'      => $friend_user_id,
+                'friend_user_name'    => $friend_user_name,
+            ),
+            array(
+                '%d',
+                '%d',
+                '%d',
+                '%s'
+            )
+        );
+        
+        /* TODO:
+         * - check each ->insert status (false means fail)
+         * - throw errors when insert fails (ideally never)
+         * - handle said errors (probably email someone)
+         * - profit?
+         */
+        return 1;
+    }
+
+
+
+
+
+
+
+
+
+		
+
+    $html = generate_table( $allBrogeyUsers);
 
     function generate_table($allBrogeyUsers) {
         $html = <<<EOHTML
@@ -91,19 +211,45 @@ EOCSS;
     <thead>
     	<tr>
     	     <th>Brogey Nation</th>
+    	     <th style="display:none">ID</th>
+    	     <th>Add?</th>
+    	     <th></th>
     	</tr>
     </thead>
     <tbody>
+	
+	<form id="add_to_favorites" method="post">
+            
 EOHTML;
-
+ 
+        
         foreach ($allBrogeyUsers as $allBrogeyUser) {
-            $allUserList = esc_html( $allBrogeyUser->user_login );
+            $allUserList = esc_html( $allBrogeyUser->user_login);
+            $allUserIds  = esc_html( $allBrogeyUser->ID);
             $html .= <<<EOHTML
+       
         <tr class="$class">
-            <td>$allBrogeyUser->user_login</td>
+            
+            <td id="friend_user_name">$allUserList</td>
+            <td id="friend_user_id" style="display:none">$allUserIds</td>
+            <td id="is_favorited">
+	            <label for="is_favorited"></label>
+		            <select id="is_favorited">
+		            	<option value=""></option>
+		            	<option value="1">Yes</option>
+		            </select>
+            </td>
+            <td>
+            	<input type="submit" onclick='ajaxFunction()' value="Add to friends" />
+            </td>
+        
         </tr>
+    	</form>
+       
+        
 EOHTML;
-        }
+	
+	}
 
     $html .= <<<EOHTML
     </tbody>
